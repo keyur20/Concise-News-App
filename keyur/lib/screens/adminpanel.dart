6cc329edeb29b073cf7195b4aafd867565b3359c
Tirth <trth25102003@gmail.com>
2024-04-19 00:31:46 +0530

    Share feature

--- keyur/lib/screens/adminpanel.dart
+++ keyur/lib/screens/adminpanel.dart
@@ -1,50 +1,62 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:share/share.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:test_2/theme_provider.dart';
-import 'package:test_2/theme_provider.dart'; // Import your theme controller
 
 class AdminPanel extends StatelessWidget {
   final user = FirebaseAuth.instance.currentUser;
 
-  signout()async{
+  signout() async {
     await GoogleSignIn().signOut();
     await FirebaseAuth.instance.signOut();
   }
-  
+
+  void shareApp() {
+    const String appLink = 'https://example.com/myapp';
+    Share.share('Check out this cool app: $appLink');
+  }
 
   @override
   Widget build(BuildContext context) {
-    // Retrieve the theme controller using Get.find
     final ThemeController themeController = Get.find<ThemeController>();
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Admin Panel'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.share),
+            onPressed: shareApp,
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Obx(() => Text(
-              'Current theme: ${themeController.isDarkTheme.value ? 'Dark' : 'Light'}',
-              style: TextStyle(fontSize: 24),
-            )),
-            SizedBox(height: 16),
-            Obx(() => Switch(
-              value: themeController.isDarkTheme.value,
-              onChanged: (value) {
-                themeController.toggleTheme();
-              },
-            )),
-          ],
-        ),
+      body: Column(
+        children: [
+          Divider(), // Line above
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Theme', style: TextStyle(fontSize: 16)),
+                Obx(() => Switch(
+                      value: themeController.isDarkTheme.value,
+                      onChanged: (value) {
+                        themeController.toggleTheme();
+                      },
+                    )),
+              ],
+            ),
+          ),
+          Divider(), // Line below
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-          onPressed: signout,
-          child: Icon(Icons.login_rounded),
-        ),
+        onPressed: signout,
+        child: Icon(Icons.logout),
+      ),
     );
   }
 }
